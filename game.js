a5d6c0ca6c18e6a8a20542aa7c1dfb6264c15648
Yusufchakkiwala <yusufchakki3@gmail.com>
2025-12-19 20:56:46 +0530

    Initial emoji game with mood text

---
+++ game.js
@@ -0,0 +1,49 @@
+const emojis=[
+    {symbol: '😀  Happy', mood: 'Happy',color:"pink"},
+    {symbol: '😢  Sad', mood: 'Sad',color:"blue"},
+    {symbol: '😠  Angry', mood: 'Angry',color:"red"},
+    {symbol: '😱  Scared', mood: 'Scared',color:"yellow"},
+    {symbol: '😎  Cool', mood: 'Cool',color:"green"},
+    {symbol: '😴  Sleepy', mood: 'Sleepy',color:"purple"},
+    {symbol: '🤔  Thinking', mood: 'Thinking',color:"orange"},
+];
+const container = document.getElementById('emoji-container');
+const message= document.getElementById('mood-message');
+//Generate emojis usiing a loop
+for( let i=0; i<emojis.length; i++){
+    const emoji=document.createElement("div");
+    emoji.className = 'emoji';
+    emoji.textContent= emojis[i].symbol;
+    emoji.onclick = function() {
+        showMood(emojis [i]);
+    };
+    container.appendChild(emoji);
+}
+   function showMood(emojiObj){
+    switch(emojiObj.mood) {
+        case 'Happy':
+            message.textContent = "Yes very Good always be happy";
+            break;
+        case 'Sad':
+            message.textContent = "Why?? then call me fast ";
+            break;
+        case 'Angry':
+            message.textContent = "Abb kya kiya maine??";
+            break;
+        case 'Scared':
+            message.textContent = "Don't be afraid, I'm here!";
+            break;
+        case 'Cool':
+            message.textContent = "you are always cool!";
+            break;
+        case 'Sleepy':
+            message.textContent = "sojaa sojaa sodtu";
+            break;
+        case 'Thinking':
+            message.textContent = "Kiske mere khyaalo me hein?";
+            break;
+        default:
+            message.textContent = `Your mood is: ${emojiObj.mood}`;
+    }
+    document.body.style.backgroundColor = emojiObj.color;
+   }
